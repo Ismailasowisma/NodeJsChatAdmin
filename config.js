04eb20c9876ba9e97116d8e119571a2be5ae9595
sow <isoronald@gmail.com>
2019-10-05 02:26:26 +0200

    TP Node js chat admin

---
+++ config.js
@@ -0,0 +1,14 @@
+// Please rename this file config.js
+
+module.exports = {
+  dbUrl:
+  "mongodb+srv://ismaila:Da081090@cluster0-lmr7i.mongodb.net/test?retryWrites=true&w=majority",
+  jwtSecret: "imaneisma",
+  server: {
+    host: "http://locahost",
+    port: {
+      api: 3000,
+      chat: 3001
+    }
+  }
+};
